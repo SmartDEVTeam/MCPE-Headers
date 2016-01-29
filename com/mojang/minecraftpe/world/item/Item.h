975c722d438a7adbe02de312cf53e6cc2b4a88e6
ThePixelGamer <thepixelmaster2354@gmail.com>
2016-01-29 17:43:00 -0600

    Added some things

--- com/mojang/minecraftpe/world/item/Item.h
+++ com/mojang/minecraftpe/world/item/Item.h
@@ -2,6 +2,46 @@
 
 class Item
 {
-    ~Item();
-    void setIcon(const std::string&, int);
+    virtual ~Item();
+    virtual Item* setIcon(const std::string&, int);
+    virtual Item* setIcon(const TextureUVCoordinateSet&);
+    virtual void setMaxStackSize(unsigned char);
+    virtual void setCategory(CreativeItemCategory);
+    virtual void setStackedByData(bool);
+    virtual void setMaxDamage(int);
+    virtual void setHandEquipped();
+    virtual void setUseAnimation(UseAnimation);
+    virtual void setMaxDuration(int);
+    virtual bool canBeDepleted();
+    virtual bool canDestroySpecial(const Block*) const;
+    virtual int getLevelDataForAuxValue(int) const;
+    virtual bool isStackedByData() const;
+    virtual int getMaxDamage();
+    virtual int getAttackDamage();
+    virtual bool isHandEquipped() const;
+    virtual bool isArmor() const;
+    virtual bool isDye() const;
+    virtual bool isFoil(const ItemInstance*) const;
+    virtual bool isThrowable() const;
+    virtual bool canDestroyInCreative() const;
+    virtual bool isLiquidClipItem(int) const;
+    virtual bool requiresInteract() const;
+    virtual const std::string& appendFormattedHovertext(const ItemInstance&, const Player&, std::string&, bool) const;
+    virtual bool isValidRepairItem(const ItemInstance&, const ItemInstance&);
+    virtual int getEnchantSlot() const;
+    virtual int getEnchantValue() const;
+    virtual bool isComplex() const;
+    virtual int getColor(const ItemInstance&) const;
+    virtual bool use(ItemInstance&, Player&);
+    virtual bool useOn(ItemInstance*, Player*, int, int, int, signed char, float, float, float);
+    virtual void dispense(BlockSource&, Container&, int, const Vec3&, signed char);
+    virtual void useTimeDepleted(ItemInstance*, Level*, Player*);
+    virtual void releaseUsing(ItemInstance*, Player*, int);
+    virtual float getDestroySpeed(ItemInstance*, Block*);
+    virtual void hurtEnemy(ItemInstance*, Mob*, Mob*);
+    virtual void interactEnemy(ItemInstance*, Mob*, Player*);
+    virtual void mineBlock(ItemInstance*, BlockID, int, int, int, Mob*);
+    virtual const std::string buildDescriptionName(const ItemInstance&) const;
+    virtual const std::string buildEffectDescriptionName(const ItemInstance&) const;
+    virtual void readUserData(ItemInstance*, IDataInput&) const;
 };
