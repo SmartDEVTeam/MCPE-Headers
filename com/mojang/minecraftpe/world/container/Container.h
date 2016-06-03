5c124cb00b9116edc87001332c0b32d448124d31
CrazyWolf23 <acooper340@gmail.com>
2016-06-03 16:53:21 -0400

    Fixed Some things to Container.h

--- com/mojang/minecraftpe/world/container/Container.h
+++ com/mojang/minecraftpe/world/container/Container.h
@@ -12,22 +12,21 @@ class std::string;
 
 class Container{
 public:
-	
 	virtual ~Container();
-	virtual void Container::addListener(ContainerListener *);
-	virtual void Container::removeListener(ContainerListener *);
-	virtual void Container::addItem(ItemInstance *);
-	virtual void Container::addItemToFirstEmptySlot(ItemInstance *);
-	virtual void Container::getRandomEmptySlot(Random &);
-	virtual void Container::dropContents(BlockSource &,Vec3 const&);
-	virtual void Container::getSlotCopies(void);
-	virtual void Container::getSlots(void);
-	virtual void Container::getItemCount(int);
-	virtual void Container::canPushInItem(int,int,ItemInstance *);
-	virtual void Container::canPullOutItem(int,int,ItemInstance *);
-	virtual void Container::setContainerChanged(int);
-	virtual void Container::setCustomName(std::string const&);
-	virtual void Container::hasCustomName(void);
-	virtual void Container::readAdditionalSaveData(CompoundTag const&);
-	virtual void Container::addAdditionalSaveData(CompoundTag &);
+	virtual void addListener(ContainerListener*);
+	virtual void removeListener(ContainerListener*);
+	virtual void addItem(ItemInstance*);
+	virtual void addItemToFirstEmptySlot(ItemInstance*);
+	virtual void getRandomEmptySlot(Random &);
+	virtual void dropContents(BlockSource&,const Vec3&);
+	virtual void getSlotCopies(void);
+	virtual void getSlots(void);
+	virtual void getItemCount(int);
+	virtual void canPushInItem(int,int,ItemInstance*);
+	virtual void canPullOutItem(int,int,ItemInstance*);
+	virtual void setContainerChanged(int);
+	virtual void setCustomName(const std::string&);
+	virtual void hasCustomName(void);
+	virtual void readAdditionalSaveData(const CompoundTag&);
+	virtual void addAdditionalSaveData(CompoundTag&);
 }
