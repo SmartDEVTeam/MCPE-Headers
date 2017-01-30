b3966e08ef6bfde76295cf8cb4334483f8bd5d7e
The Lucky Coder <razvan.filea@gmail.com>
2017-01-30 22:33:19 +0200

    Update Level related headers

--- world/level/BlockPos.h
+++ world/level/BlockPos.h
@@ -1,5 +1,13 @@
 #pragma once
 
-struct BlockPos {
+class ChunkPos;
+class Vec3;
+
+class BlockPos
+{
 	int x, y, z;
+
+	BlockPos(const ChunkPos &pos, int y);
+	BlockPos(const Vec3 &pos);
+	BlockPos(float x, float y, float z);
 };
--- world/level/dimension/Dimension.h
+++ world/level/dimension/Dimension.h
@@ -3,55 +3,89 @@
 struct CompoundTag;
 struct Packet;
 struct MoveEntityPacketData;
-struct Level;
-struct ChunkSource;
+class ChunkSource;
+class Level;
+class CircuitSystem;
+class Seasons;
+class Weather;
 
 #include "DimensionId.h"
 #include "GeneratorType.h"
-#include "../../client/renderer/Color.h"
+#include "../../util/Color.h"
 
-#include "../listeners/LevelListener.h"
+#include "../LevelListener.h"
 
-struct Dimension
-: public LevelListener
+// For MCPE 1.0.2.1
+// Size: 298
+class Dimension : public LevelListener
 {
-	char filler[84]; // 4
-	Level& level; // 88
-	char filler2[88]; // 92
+	char filler[8]; // 4
+	int fogColor; // 12
+	Level& level; // 16
+	char filler1[12]; // 20
+	float brightnessRamp; // 32
+	char filler2[80]
+	DimensionId id; // 116
+	bool ultraWarm; // 120
+	bool ceiling; // 121
+	bool isDay; // 123
+	short idk; // 124
+	char filler3[66]; // 126
+	ChunkSource* chunkSource; // 192
+	Weather* weather; // 196
+	Seasons* seasons; // 200
+	CircuitSystem* circuitSystem; // 204
 
-	Dimension(Level&, DimensionId);
+
+	Dimension(Level&, DimensionId, short);
 
 	Color getSkyColor(const Entity&, float);
 
 	std::unique_ptr<ChunkSource> _createGenerator(GeneratorType);
-	void setCeiling(bool);
-
 	static std::unique_ptr<Dimension> createNew(DimensionId, Level&);
 
 	virtual ~Dimension();
-	virtual void onBlockChanged(BlockSource&, const BlockPos&, FullBlock, FullBlock, int);
+	virtual void onBlockChanged(BlockSource&, const BlockPos&, FullBlock, FullBlock, int, Entity*);
 	virtual void onBlockEvent(BlockSource&, int, int, int, int, int);
 	virtual void onNewChunkFor(Player&, LevelChunk&);
-	virtual void init();
-	virtual void tick();
+	virtual Dimension* init();
+	virtual bool tick();
+	virtual void tickRedstone();
 	virtual void updateLightRamp();
 	virtual bool isNaturalDimension() const;
 	virtual bool isValidSpawn(int, int) const;
-	virtual Color getFogColor(float) const;
-	virtual float getFogDistanceScale() const;
+	virtual void getFogColor(float) const;
+	virtual void getFogDistanceScale() const;
 	virtual bool isFoggyAt(int, int) const;
-	virtual float getCloudHeight() const;
+	virtual void getCloudHeight() const;
 	virtual bool mayRespawn() const;
 	virtual bool hasGround() const;
-	virtual int getSpawnYPosition();
+	virtual void getSpawnPos() const;
+	virtual void getSpawnYPosition() const;
 	virtual bool hasBedrockFog();
-	virtual float getClearColorScale();
-	virtual const std::string getName() const = 0;
+	virtual void getClearColorScale();
+	virtual std::string getName() const = 0;
+	virtual void showSky() const;
+	virtual bool isDay() const;
+	virtual void translatePosAcrossDimension(const Vec3&, DimensionId) const;
 	virtual void load(const CompoundTag&);
 	virtual void save(CompoundTag&);
-	virtual void sendDimensionPackets();
 	virtual void sendBroadcast(const Packet&, Player*);
-	virtual void addMoveEntityPacket(const MoveEntityPacketData&);
-	virtual void addSetEntityMotionPacket(Entity&);
-	virtual float getTimeOfDay(int, float) const;
+	virtual void getTimeOfDay(int, float) const;
+
+	unsigned int setBrightnessRamp(unsigned int, float);
+	void setUltraWarm(bool);
+	void setCeiling(bool);
+	DimensionId getId() const;
+	Level& getLevel() const;
+	const Level& getLevelConst() const;
+	ChunkSource* getChunkSource() const;
+	CircuitSystem* getCircuitSystem() const;
+	bool isDay() const;
+	bool isNaturalDimension() const;
+	bool isFoggyAt(int, int) const;
+	bool isRedstoneTick() const;
+	bool hasGround() const;
+	bool hasCeiling() const;
+	bool hasBedrockFog() const;
 };
